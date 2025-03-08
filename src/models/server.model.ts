b9bef4002d9c7f258e4e5adf74f84f0a0331f542
fabiconcept <favourajokubi@gmail.com>
2025-03-08 18:32:43 +0100

    add owner and globalInvitationId to Server model

--- src/models/server.model.ts
+++ src/models/server.model.ts
@@ -2,8 +2,10 @@ import mongoose from "mongoose";
 
 const serverSchema = new mongoose.Schema({
     serverId: { type: String, required: true, unique: true },
+    owner: { type: String, required: true },
     serverName: { type: String, required: true },
     salt: { type: String, required: true },
+    globalInvitationId: { type: String, required: true, unique: true },
     expiresAt: { type: Date, required: true },
     createdAt: { type: Date, default: Date.now },
 });
