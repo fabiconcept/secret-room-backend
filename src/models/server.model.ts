6f34226ded9b8958d01d42cd1471a22823793d1a
fabiconcept <favourajokubi@gmail.com>
2025-03-08 01:54:46 +0100

    Update ServerSchema

--- src/models/server.model.ts
+++ src/models/server.model.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const serverSchema = new mongoose.Schema({
     serverId: { type: String, required: true, unique: true },
-    inviteLink: { type: String, required: true, unique: true },
+    serverName: { type: String, required: true },
     salt: { type: String, required: true },
     expiresAt: { type: Date, required: true },
     createdAt: { type: Date, default: Date.now },
